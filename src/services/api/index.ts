0f243434fd9c64da18ad0830bbaba2e7a734f3a0
ianfranko <ifrankie487@gmail.com>
2025-07-05 21:00:01 +0300

    Remove Post.services

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -2,6 +2,5 @@ export * from './auth.service';
 export * from './profile.service';
 export * from './sessions.service';
 export * from './tasks.service';
-export * from './posts.service';
 export * from './types';
 export * from './habits.service';
