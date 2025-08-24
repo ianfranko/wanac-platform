c68a0588c7328df2c924b3dae2bb66397b4dc983
ianfranko <ifrankie487@gmail.com>
2025-08-24 21:40:38 +0300

    outcomes page

---
+++ src/app/pages/client/mycareercompass/researchtools/outcomesindex/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OutcomesIndexPage = () => {
+  return (
+    <div>
+      <h1>Outcomes Index</h1>
+      <p>This is the Outcomes Index page.</p>
+    </div>
+  );
+};
+
+export default OutcomesIndexPage;
