df1a83530e1cf541e67b6697e571368416e097f1
ianfranko <ifrankie487@gmail.com>
2025-08-24 21:19:58 +0300

    activity stream

---
+++ src/app/pages/client/mycareercompass/activitystream/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ActivityStreamPage = () => {
+  return (
+    <div>
+      <h1>Activity Stream</h1>
+      <p>This is the Activity Stream page.</p>
+    </div>
+  );
+};
+
+export default ActivityStreamPage;
