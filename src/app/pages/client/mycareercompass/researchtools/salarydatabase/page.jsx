0d06aeb717ea587b5e99b0ddcccb061d51b659fe
ianfranko <ifrankie487@gmail.com>
2025-08-24 21:43:24 +0300

    Salary database page

---
+++ src/app/pages/client/mycareercompass/researchtools/salarydatabase/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SalaryDatabasePage = () => {
+  return (
+    <div>
+      <h1>Salary Database</h1>
+      <p>This is the Salary Database page.</p>
+    </div>
+  );
+};
+
+export default SalaryDatabasePage;
