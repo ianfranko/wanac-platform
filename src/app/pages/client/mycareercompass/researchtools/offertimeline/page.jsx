6f491fb80561bf1237df396232f1f44782670d1a
ianfranko <ifrankie487@gmail.com>
2025-08-24 21:37:57 +0300

    offertimeline

---
+++ src/app/pages/client/mycareercompass/researchtools/offertimeline/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OfferTimelinePage = () => {
+  return (
+    <div>
+      <h1>Offer Timeline</h1>
+      <p>This is the Offer Timeline page.</p>
+    </div>
+  );
+};
+
+export default OfferTimelinePage;
