76d84dd43ec8211fb325a54d346713a9fbdf3d2a
ianfranko <ifrankie487@gmail.com>
2025-10-06 06:33:46 +0300

    Remove jitsi-meet directory from build (React Native incompatible with Next.js)

--- next.config.ts
+++ next.config.ts
@@ -1,44 +1,17 @@
 import type { NextConfig } from "next";
-import webpack from 'webpack';
 
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
   // Exclude jitsi-meet directory from webpack compilation
-  webpack: (config, { isServer }) => {
-    // Ignore the entire jitsi-meet directory
+  webpack: (config, { webpack }) => {
+    // Ignore the entire jitsi-meet directory using webpack.IgnorePlugin
     config.plugins.push(
       new webpack.IgnorePlugin({
-        resourceRegExp: /^\.\/jitsi-meet/,
-        contextRegExp: /./,
+        resourceRegExp: /jitsi-meet/,
       })
     );
-    
-    // Add ignore pattern for jitsi-meet directory
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        checkResource(resource) {
-          return /jitsi-meet/.test(resource);
-        },
-      })
-    );
-
-    // Modify existing rules to exclude jitsi-meet
-    config.module.rules.forEach((rule) => {
-      if (rule && typeof rule === 'object' && 'test' in rule) {
-        const test = rule.test;
-        if (test && (test.toString().includes('tsx') || test.toString().includes('ts'))) {
-          if (Array.isArray(rule.exclude)) {
-            rule.exclude.push(/jitsi-meet/);
-          } else if (rule.exclude) {
-            rule.exclude = [rule.exclude, /jitsi-meet/];
-          } else {
-            rule.exclude = /jitsi-meet/;
-          }
-        }
-      }
-    });
 
     return config;
   },
